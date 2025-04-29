65661ce0299ae0dc5fa936178cf01a4410119507
RadwanHegazy <radwangaber11@gmail.com>
2025-04-29 08:44:48 +0300

    build create_mail() method

--- core/globals/test_objects.py
+++ core/globals/test_objects.py
@@ -1,19 +1,39 @@
 from users.models import User
 from rest_framework_simplejwt.tokens import AccessToken
-from uuid import uuid4, uuid1
+from uuid import uuid1
+from mail.models import Mail
+
 
 def create_user(
     username=None,
     email=None,
-    password=None
+    password=None,
+    full_name = "Test User"
 ) : 
 
     return User.objects.create_user(
         username = username if username else f"{uuid1()}",
         email = email if email else f"{uuid1()}@gmail.com",
         password = password if password else "password",
+        full_name = full_name
     )
 
+def create_mail(
+    from_ = None,
+    to = None,
+    header = "Test Header",
+    body = "Test Body".encode(),
+    is_read = False,
+    status = "okay"
+) : 
+    return Mail.objects.create(
+            sender=from_ if from_ else create_user(),
+            reciver=to if to else create_user(),
+            header=header,
+            body=body,
+            is_read=is_read,
+            status=status
+        )
 
 def create_tokens(user=None) : 
     return str(
